7ca24e6cf8a5a3b61080948f8d4e356eb5f304f3
Echevarria01 <ma.echeverria@alumno.um.edu.ar>
2025-06-06 00:35:07 -0300

    T1 completo - SoundLab

---
+++ soundlab_store/urls.py
@@ -0,0 +1,13 @@
+# soundlab_store/urls.py
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, ProductViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
