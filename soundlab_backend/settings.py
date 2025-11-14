07c8f1a42d0b934f299110ddc68f5802190a6858
Echevarria01 <ma.echeverria@alumno.um.edu.ar>
2025-11-14 00:43:11 -0300

    Fix CORS & production settings

--- soundlab_backend/settings.py
+++ soundlab_backend/settings.py
@@ -1,4 +1,3 @@
-
 import os
 from pathlib import Path
 from decouple import config
@@ -6,13 +5,14 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = 'django-insecure-x#d(7e=wnp10aong6lzdajha9zy$6u+&@hpcd2zpk46-jst0y)'
-
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "*",
+    "soundlab-store.up.railway.app",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -21,12 +21,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'corsheaders', 
+
     'soundlab_store',
     'usuario',
     'soundlab_api',
-    'corsheaders', 
-    'orders', 
+    'orders',
 ]
 
 REST_FRAMEWORK = {
@@ -40,7 +42,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 ROOT_URLCONF = 'soundlab_backend.urls'
 
@@ -68,66 +69,86 @@ TEMPLATES = [
     },
 ]
 
-ROOT_URLCONF = 'soundlab_backend.urls'
 WSGI_APPLICATION = 'soundlab_backend.wsgi.application'
 
 
+# -----------------------------
+# DATABASE (RAILWAY)
+# -----------------------------
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',  # o el nombre que te da Railway
-        'USER': 'root',     # o el que diga Railway
-        'PASSWORD': 'QsMudtIzMdpTWMoEFlLbylvgmIUGmJmG',  # tu pass
-        'HOST': 'shinkansen.proxy.rlwy.net',  # el host que te da Railway
-        'PORT': '19227',    # el puerto de Railway
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'QsMudtIzMdpTWMoEFlLbylvgmIUGmJmG',
+        'HOST': 'shinkansen.proxy.rlwy.net',
+        'PORT': '19227',
     }
 }
-
-
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-AUTH_USER_MODEL = 'usuario.Usuario'
-
-CORS_ALLOWED_ORIGINS = [
-    "https://soundlab.netlify.app",  
-]
-
-CORS_ALLOW_CREDENTIALS = True
-
-DEBUG = True
 
 MYSQL_URL = config('MYSQL_URL', default=None)
 if MYSQL_URL:
     DATABASES['default'] = dj_database_url.parse(MYSQL_URL, conn_max_age=600)
+
+
+# -----------------------------
+# PASSWORD VALIDATION
+# -----------------------------
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+
+# -----------------------------
+# STATIC & MEDIA
+# -----------------------------
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'usuario.Usuario'
+
+
+# -----------------------------
+# CORS CONFIG (NETLIFY ↔ RAILWAY)
+# -----------------------------
+CORS_ALLOWED_ORIGINS = [
+    "https://soundlabstore.netlify.app",
+    "https://soundlab-store.up.railway.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://soundlabstore.netlify.app",
+    "https://soundlab-store.up.railway.app",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "authorization",
+    "content-type",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+
+
+
+
