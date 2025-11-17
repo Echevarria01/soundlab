633a2314eab49b0b8361b39b1c38bbf7c7ff5de6
Echevarria01 <ma.echeverria@alumno.um.edu.ar>
2025-11-17 11:45:37 -0300

    arreglos pedidos

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-from soundlab_store.views import OrderListCreateView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import OrderViewSet
+
+router = DefaultRouter()
+router.register(r'', OrderViewSet, basename='orders')
 
 urlpatterns = [
-    path('', OrderListCreateView.as_view(), name='order-create'),
+    path('', include(router.urls)),
 ]
 
 
